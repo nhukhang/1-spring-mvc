f7de828702d3561f14db72b738ce5d18dea47e4b
khangdev@gmail.com <khangdev@gmail.com@393264c8-d56d-baa5-7c66-28bd2573f499>
2013-08-26 15:30:17 +0000

    Initial Project

---
+++ trunk/Spring/trunk/app/src/main/java/App.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nkhang
+ * Date: 8/26/13
+ * Time: 10:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.print("Hello");
+    }
+}
