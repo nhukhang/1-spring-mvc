9b1a94055c00a892dcdea38e32131692b70e7101
khangdev@gmail.com <khangdev@gmail.com@393264c8-d56d-baa5-7c66-28bd2573f499>
2013-10-11 10:54:46 +0000

    JMS - Integration

--- trunk/Spring/trunk/crawler/src/main/java/Run.java
+++ trunk/Spring/trunk/crawler/src/main/java/Run.java
@@ -1,14 +1,18 @@
 
 import com.springapp.common.dto.CategoryDTO;
 import com.springapp.service.impl.CategoryServiceImpl;
+import org.apache.activemq.command.ActiveMQDestination;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
 
 
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
 import java.io.IOException;
 import java.util.List;
 
@@ -21,15 +25,32 @@ import java.util.List;
  */
 public class Run {
     public static void main(String[] args){
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-services.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
         CategoryServiceImpl categoryService = (CategoryServiceImpl)context.getBean("homeCategory");
+        JmsTemplate template = (JmsTemplate) context.getBean("jmsTemplate");
+        ActiveMQDestination destination = (ActiveMQDestination) context.getBean("destination");
+
         try{
             List<CategoryDTO> categoryDTOs = categoryService.getMainCategory();
             for(CategoryDTO categoryDTO : categoryDTOs){
-                System.out.println(categoryDTO.getName()+"  "+categoryDTO.getUrl());
+                template.convertAndSend(destination, categoryDTO.getUrl());
+                Thread.sleep(1000);
+
+                // receiving a message
+                Object msg = template.receive(destination);
+                if (msg instanceof TextMessage) {
+                    try {
+                        System.out.println(((TextMessage) msg).getText());
+                    } catch (JMSException e) {
+                        System.out.println(e);
+                    }
+                }
             }
         }catch (Exception e){
             System.out.print(e.getMessage());
         }
+
+
     }
 }
